9d9506b22372b41695f2e554266ffa62c8ba9f94
Dima.KItsenko <dima.kitsenko2001@gmail.com>
2025-10-26 15:52:19 +0000

    add all

---
+++ app/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <header class="h-[70px] bg-[#E4E4E4] flex items-center">
+        <div class="w-[60px] h-[60px] ml-[16px] bg-[#C4C4C4] rounded-full"></div>
+        <div class="ml-auto h-[70px] flex">
+            <NuxtLink to="/" class="px-7 py-7 h-[70px] bg-[#D5D5D5] flex items-center">
+                Home                        
+            </NuxtLink>   
+            <NuxtLink to="/about" class="px-7 py-7 h-[70px] hover:bg-[#DBDBDB] flex items-center">
+                Options                        
+            </NuxtLink>
+            <NuxtLink to="/news" class="px-7 py-7 h-[70px] hover:bg-[#DBDBDB] flex items-center">
+                Contact Us                        
+            </NuxtLink>
+        </div>
+    </header>
+    <div class="w-full h-[484px] bg-[#C4C4C4]"></div>
+
+  <main>
+    <slot />
+  </main>
+
+  <footer class="h-[70px] bg-[#E4E4E4] flex items-center">
+    <div class="w-[60px] h-[60px] bg-[#C4C4C4] rounded-full ml-[21px]"></div>
+    <div class="w-[60px] h-[60px] bg-[#C4C4C4] rounded-full ml-[21px]"></div>
+    <div class="w-[60px] h-[60px] bg-[#C4C4C4] rounded-full ml-[21px]"></div>
+    <div class="ml-auto">
+      <h1 class="ml-[71px] w-[608px] text-[24px] font-bold">@ 2022  Ar Filters Majito - Maria José Mercau</h1>
+    </div>
+  </footer>
+
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-full h-[526px] flex items-center justify-center">
+        <div class="w-[996px] h-[301px] flex">
+            <img src="/ico1.png" alt="ico1">
+            <div class="w-full h-full">
+                <h1 class="ml-[71px] w-[608px] text-[24px] font-bold">Hi, I'm Majo and I create custom Instagram and Facebook Filters!</h1>
+                <p class="mt-[40px] ml-[71px]">
+                    Whether you’re just starting to dip your feet in the world of filtering your photographs or trying to up grade your game at social media, you have come to the right place! <br>
+                    <br>
+                    Also known as the Augmented Reality (AR) filter, the effect is placed over the picture and videos you take on Facebook and Instagram to give it a pleasant and vibrant look.br <br>
+                    <br>
+                    Keep scrolling to learn how can we help you!
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="w-full h-[526px] flex items-center justify-center">
+        <div class="w-[996px] h-[335px] flex">
+            <div class="w-full h-full">
+                <h1 class="w-[608px] mt-[60px] text-[48px] font-bold">WHY?</h1>
+                <p class="w-[568px] mt-[22px] text-[18px]">
+                Instagram and Facebook filters, without a doubt, add a creative touch to your stories, surprising and enchanting your followers. But they are also an excellent tool for brand positioning and establishing yourself on social media.
+                </p>
+            </div>
+            <div class="w-[171px] h-full bg-[#C4C4C4]"></div>
+        </div>
+    </div>
+</template>
